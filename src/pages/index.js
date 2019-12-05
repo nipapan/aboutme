f3f2efa00acccfd5df15afa96a24651329384127
Nipapan Aom <nipapan@maqe.com>
2019-12-05 13:56:35 +0700

    Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import './mystyles.scss';
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -10,7 +12,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
